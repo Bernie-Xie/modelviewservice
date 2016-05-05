cc544be6bb918a5de5a22a7de370012c84b4d8ad
Will <chopin.champagne@gmail.com>
2016-05-05 20:46:16 +0800

    Add more HandlerAdapter to support POST

--- src/main/java/com/inno/modelview/configuration/CORSFilter.java
+++ src/main/java/com/inno/modelview/configuration/CORSFilter.java
@@ -1,6 +1,7 @@
 package com.inno.modelview.configuration;
 
 import java.io.IOException;
+import java.util.HashMap;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,18 +13,34 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CORSFilter implements Filter {
 
+    final static String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    final static String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    final static String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    final static String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    final static String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+
+    private HashMap<String, String> responseSettings = new HashMap();
+
+    {
+        responseSettings.put(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        responseSettings.put(ACCESS_CONTROL_ALLOW_CREDENTIALS, "*");
+        responseSettings.put(ACCESS_CONTROL_ALLOW_METHODS, "POST, GET, PUT, OPTIONS, DELETE");
+        responseSettings.put(ACCESS_CONTROL_MAX_AGE, "172800");
+        responseSettings.put(ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name");
+    }
+
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         //TODO Need to replace with DNS when in the production,
         //Otherwise it would be extremely dangerous that every unknown request may process this API
         //e.g. http://localhost:3000
         //http://192.168.18.145:3000
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, responseSettings.get(ACCESS_CONTROL_ALLOW_ORIGIN));
+        response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, responseSettings.get(ACCESS_CONTROL_ALLOW_CREDENTIALS));
+        response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, responseSettings.get(ACCESS_CONTROL_ALLOW_METHODS));
         //Beyond the timeRange (Access-Control-Max-Age), preflight requests would send "OPTION" again to the API to make sure the safety
-        response.setHeader("Access-Control-Max-Age", "1728000");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader(ACCESS_CONTROL_MAX_AGE, responseSettings.get(ACCESS_CONTROL_MAX_AGE));
+        response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, responseSettings.get(ACCESS_CONTROL_ALLOW_HEADERS));
         chain.doFilter(req, res);
         //TODO add Cache
     }
